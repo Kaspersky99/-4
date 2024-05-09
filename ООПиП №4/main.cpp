6a8f47b2d8be914df66c765dbec49a4512f68581
Kasper <dima5700977@gmail.com>
2024-05-09 10:35:42 +0300

    Initial Commit

---
+++ ООПиП №4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ООПиП №4
+//
+//  Created by Kasper on 5/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
